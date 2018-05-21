f9d2e98a8aaf44f5bd2071fc588d285184259a27
Hguimaraes <hguimaraes@poli.ufrj.br>
2018-05-20 21:37:40 -0300

    subtraction module

---
+++ mULA/m2CMPLMT.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Create Date:    21:12:00 05/20/2018 
+-- Description: 
+--      Module to calculate the two's complement of A (4-bit)
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+--  Módulo para realizar o complemento a 2 de um vetor
+entity m2CMPLMT is
+    Port (A : in  STD_LOGIC_VECTOR(3 downto 0);
+          X : out  STD_LOGIC_VECTOR(3 downto 0);
+			 COUT: out STD_LOGIC_VECTOR(3 downto 0));
+end m2CMPLMT;
+
+architecture Behavioral of m2CMPLMT is
+
+signal notA:std_logic_vector(3 downto 0);
+
+-- Componente de Negação de um vetor de 4 Bits
+component mNOT
+	port(A: in std_logic_vector(3 downto 0);
+			X: out std_logic(3 downto 0));
+end component;
+
+-- Componente para somar +1 ao vetor negado e obter o complemento a 2
+component mADD
+	Port ( A,B : in  STD_LOGIC_VECTOR(3 downto 0);
+           c_in : in  STD_LOGIC;
+           X : out  STD_LOGIC_VECTOR(3 downto 0);
+           c_out : out  STD_LOGIC);
+end component;
+
+signal ONE:STD_LOGIC_VECTOR(3 DOWNTO 0);
+signal CIN: STD_LOGIC;
+
+begin
+	ONE <= "0001";
+	CIN <= '0';
+	
+	M2C01: mNOT port map(A, notA);
+	M2C02: mADD port map(notA, ONE, CIN, X, COUT);
+
+end Behavioral;
---
+++ mULA/mSUB.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Create Date:    20:30:00 05/20/2018 
+-- Description: 
+--      Module X = A - B (4 bits)
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Subtraction entity
+entity mSUB is
+    Port ( A : in  STD_LOGIC_VECTOR(3 downto 0);
+           B : in  STD_LOGIC_VECTOR(3 downto 0);
+           X : out  STD_LOGIC_VECTOR(3 downto 0);
+			  Brw : out STD_LOGIC_VECTOR(3 downto 0));
+end mSUB;
+
+architecture Behavioral of mSUB is
+
+signal m2comp: STD_LOGIC_VECTOR(3 downto 0);
+signal cin: STD_LOGIC;
+signal cout: STD_LOGIC;
+
+-- 4-bit adder
+component mADD
+	Port ( A,B : in  STD_LOGIC_VECTOR(3 downto 0);
+           c_in : in  STD_LOGIC;
+           X : out  STD_LOGIC_VECTOR(3 downto 0);
+           c_out : out  STD_LOGIC);
+end component;
+
+-- Two's complement
+component m2CMPLMT
+    Port (A : in  STD_LOGIC_VECTOR(3 downto 0);
+          X : out  STD_LOGIC_VECTOR(3 downto 0);
+			 COUT: out STD_LOGIC_VECTOR(3 downto 0));
+end component;
+
+begin
+	-- CIN adder
+	cin <= '0';
+	
+	-- A + m2CMPLMT(B)
+	SUB001: m2CMPLMT port map(B, m2comp, cout);
+	SUB002: mADD port map(A, m2comp, cin, X, Brw);
+	Brw <= NOT Brw;
+
+end Behavioral;
